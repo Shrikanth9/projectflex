871bdc2c3afae43418b50b645f223eea9bff6753
Shrikanth <shrikanhbhat30@gmail.com>
2025-06-28 13:33:34 +0530

    Initial commit - Project setup

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      'uploadthing.com',
+    ]
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
